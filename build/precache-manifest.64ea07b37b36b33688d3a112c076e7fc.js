a84a6051095467e81a970338fa27cce079edb4ba
tarunkumarpatel02 <tarunkpatel02@gmail.com>
2020-10-19 09:58:21 +0530

    deploying to gh-pages

---
+++ build/precache-manifest.64ea07b37b36b33688d3a112c076e7fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3397812efd1a58dec27d79c672016d8",
+    "url": "/Arkenea_assignment/index.html"
+  },
+  {
+    "revision": "fa8ba0ec44c8afe62b21",
+    "url": "/Arkenea_assignment/static/css/main.555e86bf.chunk.css"
+  },
+  {
+    "revision": "48d1074534d7416b503d",
+    "url": "/Arkenea_assignment/static/js/2.baa0b4bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Arkenea_assignment/static/js/2.baa0b4bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8ba0ec44c8afe62b21",
+    "url": "/Arkenea_assignment/static/js/main.47a32b3c.chunk.js"
+  },
+  {
+    "revision": "b4a9cfa9dd7a44c2fe59",
+    "url": "/Arkenea_assignment/static/js/runtime-main.c6b3a033.js"
+  }
+]);
